807215bf97b4fde8315cbc4de08d24bf5fb41a86
Adrian Cudapas <adriancudapas08@gmail.com>
2024-02-18 00:32:28 +0800

    API added

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 
-const login = require('./myAPI.json')
+const login = require('./login.json')
+const register = require('./register.json')
+const validate = require('./validate-unique-email.json')
+const profileGet = require('./profile.json')
+const profile = require('./profileGet.json')
 
 const port = process.env.PORT || 3000
 
@@ -9,8 +13,25 @@ app.get('/', (req, res) => {
     res.send("MY API")
 })
 
-app.get('/login', (req, res) =>{
+app.post('/login', (req, res) =>{
     res.send(login)
+})
+
+app.post('/register', (req, res) =>{
+    res.send(register)
+})
+
+app.post('/validate', (req, res) =>{
+    res.send(validate)
+})
+
+
+app.post('/profile', (req, res) =>{
+    res.send(profile)
+})
+
+app.get('/profile', (req, res) =>{
+    res.send(profileGet)
 })
 
 
